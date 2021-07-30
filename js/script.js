ab0bb78e4ee2bd3cfb86c9f56c7469e15a42527f
Yannick Kamdem <yannickkamdemkouam@yahoo.fr>
2021-07-30 21:29:39 +0200

    Add comments

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ let dot2 = document.getElementById("dot-2")
 
 let count = 1
 
+// Changement de background en fonction du joueur qui joue
 
 let setTheme = () => {
   if (count % 2 != 1) {
@@ -39,6 +40,9 @@ let setTheme = () => {
   }
 }
 
+
+// Initialisation et vérification du joueur en cours
+
 let setPlayer = () => {
   if (count % 2 == 1) {
     scoring = scoring1
@@ -51,6 +55,8 @@ let setPlayer = () => {
 }
 
 setPlayer()
+
+// Vérification du joueur qui dépasse 100 pts
 
 let winner = () => {
   if (parseInt(scorePlayer.innerHTML, 10) >= 100) {
@@ -68,43 +74,41 @@ let winner = () => {
   }
 }
 
+// Incrémentation du score global avec le score courant 
+
 let setTotalScore = (total) => {
-  let value = scorePlayer.innerHTML
-  let stringToInt = parseInt(value, 10)
-  stringToInt += total
-  scorePlayer.innerHTML = stringToInt
+  let value = parseInt(scorePlayer.innerHTML, 10)
+  value += total
+  scorePlayer.innerHTML = value
   winner()
 }
 
-let calculTotalScore = () => {
-  let value = scoring.innerHTML
-  let stringToInt = parseInt(value, 10)
-  total = stringToInt
-  return total
-}
+
+// Incrémentation du score courant avecle score obtenu sur le dé
 
 let setCurrentScore = (number) => {
-  let value = scoring.innerHTML
-  let stringToInt = parseInt(value, 10)
-  stringToInt += number
-  scoring.innerHTML = stringToInt
+  let value = parseInt(scoring.innerHTML, 10)
+  value += number
+  scoring.innerHTML = value
 }
 
-let roll = () => {
-  let rollDice = document.getElementsByTagName("box")
 
-  rollDice.classList.remove("")
-  rollDice.classList.add("")
-}
+// Modification de la valeur du dé
 
 let changeDice = (number) => {
   dice.removeAttribute("src")
   dice.setAttribute("src", "dado_" + number + ".svg")
 }
 
+
+// Lancement d'une nouvelle partie
+
 newGame.addEventListener("click", () => {
   document.location.reload()
 })
+
+
+// Génération d'une valeur aléatoire entre 1 et 6 grace a un évènement clic
 
 rollDice.addEventListener("click", () => {
   let number = Math.floor(Math.random()*6)
@@ -124,8 +128,11 @@ rollDice.addEventListener("click", () => {
   }
 })
 
+
+// Ajout du score courant au global avec changement de joueur
+
 hold.addEventListener("click", () => {
-  let total = calculTotalScore()
+  let total = parseInt(scoring.innerHTML, 10)
   setTotalScore(total)
   if (scoring.innerHTML == 0) {
     alert("Votre score courant est de 0, Vous devez lancer le dé avant d'enregistrer")
